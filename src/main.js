4da7460f4287deb6b22a6932104a875544be9d46
BaileysIrish <139779857+BaileysIrish@users.noreply.github.com>
2023-07-25 22:41:18 +0900

    JS-Projects

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector(".header");
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -10,12 +11,14 @@ document.addEventListener("scroll", () => {
   }
 });
 
+//Home 섹션을 아래로 스크롤시 투명하게 처리
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// Arrow up버튼을 아래로 스크롤시 투명하게 처리
 const arrow = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
@@ -25,12 +28,14 @@ document.addEventListener("scroll", () => {
   }
 });
 
+// Navbar 토글버튼 클릭 처리
 const navbarMenu = document.querySelector(".header__menu");
 const navbarToggle = document.querySelector(".header__toggle");
 navbarToggle.addEventListener("click", () => {
   navbarMenu.classList.toggle("open");
 });
 
+// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
 navbarMenu.addEventListener("click", () => {
   navbarMenu.classList.remove("open");
 });
